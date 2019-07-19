00e8b6216afe67205f2f7d4dcee2cbb9b4837bc6
umbur <you@example.com>
2019-07-19 16:10:34 -0600

    working on render fix

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import StarWars from './components/StarWars.js'
 import './App.css';
 
 const App = () => {
@@ -8,10 +10,20 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState()
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people/')
+    .then(res => {
+      setData(res.data.results);
+      console.log('res', res.data.results)
+    })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <StarWars name={data.name} />
+      {/* { data.results.map(x => { return <StarWars name={data.name} /> })  } */}
     </div>
   );
 }
---
+++ starwars/src/components/StarWars.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function StarWars(props) {
+    //console.log(props)
+    return(
+        <div>
+            <h2>{props.name}</h2>
+            
+        </div>
+    )
+}
+
+export default StarWars;
